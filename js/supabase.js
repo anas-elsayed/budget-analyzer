a1388bf578bffe3230fa23035c78c80c153612bb
anas-elsayed <85259053+anas-elsayed@users.noreply.github.com>
2026-05-12 17:05:24 +0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,8 @@
 
 // PASTE YOUR SUPABASE VALUES HERE
 
-const SUPABASE_URL = "PASTE_SUPABASE_URL";
-const SUPABASE_KEY = "PASTE_SUPABASE_ANON_KEY";
+const SUPABASE_URL = "https://mwtbqxycinvcipiizoqn.supabase.co";
+const SUPABASE_KEY = "sb_publishable_EaJYZ5gN6CUh6iGfVV5juA_qjui6B5b";
 
 export const supabase = window.supabase.createClient(
   SUPABASE_URL,
